aca5c7bcd57535cb880f75fb77614cc7caf611b0
gregoriodemelo <julianomelogregorio@gmail.com>
2026-04-29 22:25:50 +0000

    add camadas

--- js/script.js
+++ js/script.js
@@ -1,25 +1,24 @@
+// === ESTADO (dados da aplicação) === 
 let likeCount = 0;
 let dislikeCount = 0;
 let curtido = false;
 let descurtido = false;
 
+// === SERVICE (regras de negócio)
 function curtir() {
   if (curtido == false){
     likeCount++;
     curtido = true;
-    document.getElementById("likeCount").innerText = likeCount;
+    
 
     if(descurtido == true){
       dislikeCount--;
       descurtido = false;
-      document.getElementById("dislikeCount").innerText = dislikeCount;
-
     }
 
   }else{
     likeCount--;
     curtido = false;
-    document.getElementById("likeCount").innerText = likeCount;
   }
 
 }
@@ -28,23 +27,43 @@ function descurtir() {
   if(descurtido == false){
     dislikeCount++;
     descurtido = true;
-    document.getElementById("dislikeCount").innerText = dislikeCount;
+    
 
     if(curtido == true){
       likeCount--;
       curtido = false;
-      document.getElementById("likeCount").innerText = likeCount;
     }
 
   }
   else{
     dislikeCount--;
     descurtido = false;
-    document.getElementById("dislikeCount").innerText = dislikeCount;
   }
 }
 
+//=== VIEWS (interface)===
+
+function atualizarTela(){
+  document.getElementById("likeCount").innerText = likeCount;
+  document.getElementById("dislikeCount").innerText = dislikeCount;
+}
+
+//=== CONTROLLER (intermediação evento/regras de negócio)
+
+function clicarCurtir(){
+  curtir();
+  atualizarTela();
+}
+function clicarDescurtir(){
+  descurtir();
+  atualizarTela();
+}
 
 
-document.getElementById("likeBtn").addEventListener("click", curtir);
-document.getElementById("dislikeBtn").addEventListener("click", descurtir);
+// === EVENTOS ====
+document.getElementById("likeBtn").addEventListener("click", clicarCurtir);
+document.getElementById("dislikeBtn").addEventListener("click", clicarDescurtir);
+
+// === INICIALIZAÇÃO DE TELA ===
+
+atualizarTela();
