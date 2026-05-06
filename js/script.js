bfd175b22d88ab356957bc467983a403b21cd1b5
gregoriodemelo <julianomelogregorio@gmail.com>
2026-05-06 14:16:35 +0000

    Update atualizar tela

--- js/script.js
+++ js/script.js
@@ -11,19 +11,15 @@ function curtir() {
   if (curtido == false){
     likeCount++;
     curtido = true;
-    document.getElementById("likeCount").innerText = likeCount;
 
     if(descurtido == true){
       dislikeCount--;
       descurtido = false;
-      document.getElementById("dislikeCount").innerText = dislikeCount;
-
     }
 
   }else{
     likeCount--;
     curtido = false;
-    document.getElementById("likeCount").innerText = likeCount;
   }
 
 }
@@ -32,32 +28,41 @@ function descurtir() {
   if(descurtido == false){
     dislikeCount++;
     descurtido = true;
-    document.getElementById("dislikeCount").innerText = dislikeCount;
 
     if(curtido == true){
       likeCount--;
       curtido = false;
-      document.getElementById("likeCount").innerText = likeCount;
     }
 
   }
   else{
     dislikeCount--;
     descurtido = false;
-    document.getElementById("dislikeCount").innerText = dislikeCount;
   }
+}
+
+// === VIEW (interface/renderização)===
+function atualizarTela(){
+  document.getElementById("likeCount").innerText = likeCount;
+  document.getElementById("dislikeCount").innerText = dislikeCount;
 }
 
 //=== CONTROLLER (intermediação)===
 
 function clicarCurtir(){
   curtir();
+  atualizarTela();
 }
 function clicarDescurtir(){
   descurtir();
+  atualizarTela();
 }
 
 // === EVENTOS ===
 
 document.getElementById("likeBtn").addEventListener("click", clicarCurtir);
 document.getElementById("dislikeBtn").addEventListener("click", clicarDescurtir);
+
+// === INICIALIZAÇÃO ===  
+
+atualizarTela();
